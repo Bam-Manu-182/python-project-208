d62312a49ecf771ed22a9156842e61dedaf45edd
Bam-Manu-182 <bammanuel182@gmail.com>
2026-07-24 20:58:18 -0500

    Project 208 V1 Commit 2
    Se realizan los pasos acorde a lo solicitado en el proyecto así:
    
    Se configura Bootstrap y se usan las plantillas HTML y vista principal
    
    Se procede a Instalar y registrar django-bootstrap5 en el proyecto.
    Se configura la ruta de plantillas (templates) en settings.py.
    La primera es base.html con barra de navegación de Bootstrap (Gestor de tareas, Usuarios, Iniciar sesión, Registrarse).
    La segunda es index.html extendiendo de base.html con el mensaje de bienvenida.
    Además se actualiza urls.py para renderizar la plantilla principal en la ruta '/'.
    
    Se realizan las pruebas internas para verificar estado y vista, queda funcional.
    
    Con esto queda listo es paso 2 del Proyecto.

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -16,15 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 
-def home_view(request):
-    return HttpResponse("Bienvenido a Task Manager!")
+def index(request):
+    return render(request, 'index.html')
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_view),
+    path('', index),
 ]
