a177a06d7826f4d39471f92bfbd839bd7b75939e
Bam-Manu-182 <bammanuel182@gmail.com>
2026-08-03 21:17:42 -0500

    Tests 12
    Tests para Hexlet

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -98,7 +98,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.MD5PasswordHasher',
-    'django.contrib.auth.hashers.Argon2PasswordHasher',
     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
 ]
 
