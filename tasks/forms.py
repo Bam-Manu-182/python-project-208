6bcbd2a89a65356d472fba343fca0449f2929ab6
Bam-Manu-182 <bammanuel182@gmail.com>
2026-08-05 20:05:00 -0500

    Tests 28
    Correcciones para tests de Hexlet

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,8 +1,22 @@
 from django import forms
 from .models import Task
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+class UserChoiceField(forms.ModelChoiceField):
+    def label_from_instance(self, obj):
+        return obj.get_full_name() or obj.username
 
 
 class TaskForm(forms.ModelForm):
+    executor = UserChoiceField(
+        queryset=User.objects.all(),
+        required=False,
+        label='Ejecutor'
+    )
+
     class Meta:
         model = Task
         fields = ['name', 'description', 'status', 'executor', 'labels']
