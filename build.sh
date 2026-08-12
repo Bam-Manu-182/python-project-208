577563cd5c4c56797c16a8f0f806212644ed63da
Bam-Manu-182 <bammanuel182@gmail.com>
2026-08-11 22:16:32 -0500

    Correcciones finales 4.4
    Se crea archivo de conexión con qlty.sh se configura para la revisión del coverage.

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ set -o errexit
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
 # shellcheck disable=SC1091
-source $HOME/.local/bin/env
+source "$HOME/.local/bin/env"
 
 make install && make collectstatic && make migrate
